88f6925ebf53d7a7941f9aba8cdf834d41137d49
Thomas-Pronovost92 <pronovost.t>
2025-02-11 17:40:18 -0500

    bulk

---
+++ td2_partie2/ListeFilms.cpp
@@ -0,0 +1,69 @@
+#include "structures.hpp"
+#include "cppitertools/range.hpp"
+#include "iostream"
+
+using namespace std;
+using namespace iter;
+
+ListeFilms::ListeFilms():
+	ListeFilms(1, 0, new Film* [1]) {
+}
+
+ListeFilms::ListeFilms(int capacite, int nElements, Film** elements):
+	capacite_(capacite), nElements_(nElements), elements_(elements) {
+}
+
+void ListeFilms::agrandirListeFilm() {
+	int nouvelleCapacite = 2 * capacite_;
+	Film** nouvelleListeFilms = new Film * [nouvelleCapacite];
+
+	for (int filmIndex : range(0, nElements_)) {
+		nouvelleListeFilms[filmIndex] = elements_[filmIndex];
+	}
+
+	delete[] elements_;
+	elements_ = nouvelleListeFilms;
+	capacite_ = nouvelleCapacite;
+}
+
+//TODO: Une fonction pour ajouter un Film à une ListeFilms, le film existant déjà; on veut uniquement ajouter le pointeur vers le film existant.  Cette fonction doit doubler la taille du tableau alloué, avec au minimum un élément, dans le cas où la capacité est insuffisante pour ajouter l'élément.  Il faut alors allouer un nouveau tableau plus grand, copier ce qu'il y avait dans l'ancien, et éliminer l'ancien trop petit.  Cette fonction ne doit copier aucun Film ni Acteur, elle doit copier uniquement des pointeurs.
+void ListeFilms::ajouterFilm(Film* filmPtr) {
+	if (nElements_ == capacite_) {
+		agrandirListeFilm();
+	}
+
+	int indexNouvelElement = nElements_;
+	elements_[indexNouvelElement] = filmPtr;
+	nElements_++;
+}
+
+
+//TODO: Une fonction pour enlever un Film d'une ListeFilms (enlever le pointeur) sans effacer le film; la fonction prenant en paramètre un pointeur vers le film à enlever.  L'ordre des films dans la liste n'a pas à être conservé.
+void ListeFilms::enleverFilm(Film* filmAEnleverPtr) {
+	for (int filmIndex : range(0, nElements_)) {
+		Film* filmPtr = elements_[filmIndex];
+		if (filmAEnleverPtr == filmPtr) {
+			Film* dernierFilmPtr = elements_[nElements_ - 1];
+			elements_[filmIndex] = dernierFilmPtr;
+			nElements_--;
+		}
+	}
+}
+
+
+span<Film*> ListeFilms::spanListeFilms() const {
+	return span<Film*>(elements_, nElements_);
+}
+
+
+int ListeFilms::obtenirNElements() const {
+	return nElements_;
+}
+
+void ListeFilms::libererTableauFilm() {
+	delete[] elements_;
+}
+
+Film* ListeFilms::obtenirPremierFilmPtr() const {
+	return elements_[0];
+}
---
+++ td2_partie2/structures.hpp
@@ -0,0 +1,54 @@
+#pragma once
+// Structures mémoires pour une collection de films.
+
+#include <string>
+#include <span>
+#include <fstream>
+
+struct Film; struct Acteur; struct ListeActeurs; // Permet d'utiliser les types alors qu'ils seront défini après.
+
+/*
+struct ListeFilms {
+	int capacite, nElements;
+	Film** elements; // Pointeur vers un tableau de Film*, chaque Film* pointant vers un Film.
+};*/
+
+class ListeFilms {
+public:
+	ListeFilms();
+	ListeFilms(int capacite, int nElements, Film** elements);
+
+	void ajouterFilm(Film* filmPtr);
+	void enleverFilm(Film* filmAEnleverPtr);
+
+	std::span<Film*> spanListeFilms() const;
+
+	void libererTableauFilm();
+
+	int obtenirNElements() const;
+	Film* obtenirPremierFilmPtr() const;
+
+private:
+	int capacite_, nElements_;
+	Film** elements_; // Pointeur vers un tableau de Film*, chaque Film* pointant vers un Film.
+
+	void agrandirListeFilm();
+};
+
+struct ListeActeurs {
+	int capacite, nElements;
+	Acteur** elements; // Pointeur vers un tableau de Acteur*, chaque Acteur* pointant vers un Acteur.
+};
+
+struct Film
+{
+	std::string titre, realisateur; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
+	int anneeSortie, recette; // Année de sortie et recette globale du film en millions de dollars
+	ListeActeurs acteurs;
+};
+
+struct Acteur
+{
+	std::string nom; int anneeNaissance; char sexe;
+	ListeFilms joueDans;
+};
